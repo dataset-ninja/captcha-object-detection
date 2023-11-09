9d36c820b3e75e33d367794137c28449a57accca
erastov-alex <erastov.alex@gmail.com>
2023-11-09 06:47:41 +0300

    upd convert, settings, gitignore

--- src/settings.py
+++ src/settings.py
@@ -13,8 +13,8 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Captcha Object Detection"
+PROJECT_NAME_FULL: str = "Captcha Object Detection"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
